750f8c3c69362b795bcd281f3725265f1bf117fd
IcemanHHW <maartenkampmeijer@hotmail.com>
2017-06-14 15:02:07 +0200

    Add files via upload
    aanpassingen voor de live versie

--- myband/admin/config.php
+++ myband/admin/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST','localhost');
-define('DB_NAME','myband');
-define('DB_USERNAME','22617');
-define('DB_PASSWORD','cnibcu');
+define('DB_NAME','kampmeijer_nl_school');
+define('DB_USERNAME','kampm_nl_school');
+define('DB_PASSWORD','96LbAXakhX3v');
