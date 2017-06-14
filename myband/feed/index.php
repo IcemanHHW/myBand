3339eac6e35e89f455893111e04ddab3b60aea1c
IcemanHHW <maartenkampmeijer@hotmail.com>
2017-06-14 15:02:43 +0200

    Add files via upload
    aanpassingen voor de live versie en bugs oplossen

--- myband/feed/index.php
+++ myband/feed/index.php
@@ -1,17 +1,14 @@
 <?php
     header("Content-Type: application/rss+xml; charset=ISO-8859-1");
 
-    define('DB_HOST','localhost');
-    define('DB_NAME','myband');
-    define('DB_USERNAME','22617');
-    define('DB_PASSWORD','cnibcu');
+    require_once ('../admin/config.php');
 
-    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+    $rssfeed .= '<?xml version="1.0" encoding="ISO-8859-1"?>';
     $rssfeed .= '<rss version="2.0">';
     $rssfeed .= '<channel>';
     $rssfeed .= '<title>RSS Feed MY Band</title>';
     $rssfeed .= '<link>http://ma.kampmeijer.nl/myband/</link>';
-    $rssfeed .= '<p>RSS Feed from MY Band</description>';
+    $rssfeed .= '<description>RSS Feed from MY Band</description>';
     $rssfeed .= '<language>en-us</language>';
 
     $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME );
@@ -26,9 +23,9 @@
         extract($row);
 
         $rssfeed .= '<item>';
-        $rssfeed .= '<title>' . $title . '</title><br />';
-        $rssfeed .= '<description>' . $content . '</description><br />';
-        $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($date)) . '</pubDate><br />';
+        $rssfeed .= '<title>' . $row['title'] . '</title>';
+        $rssfeed .= '<description>'  . $row['content'] . '</description>';
+        $rssfeed .= '<pubDate>' . $row['date'] . '</pubDate>';
         $rssfeed .= '</item>';
     }
 
