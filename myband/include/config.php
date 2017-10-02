130c5663d861686909fc6c3838488878448230c9
IcemanHHW <maartenkampmeijer@hotmail.com>
2017-10-02 20:25:42 +0200

    Delete config.php

--- myband/include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$title = 'MY Band';
-
-define('DB_HOST','localhost');
-define('DB_NAME','kampmeijer_nl_school');
-define('DB_USERNAME','kampm_nl_school');
-define('DB_PASSWORD','96LbAXakhX3v');
-
-?>
